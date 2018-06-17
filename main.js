385bc9cc211525178c641453d4977d65bbcf2a84
helloyongwei <2628578405@qq.com>
2018-06-17 18:16:32 +0800

    add full files

---
+++ main.js
@@ -0,0 +1,17 @@
+let view = new Vue({
+  el: '#app',
+  data: {
+    open: false
+  },
+  template: `
+    <div>
+      <button v-on:click="toggle">点我</button>
+      <div v-if="open">你好</div>
+    </div>
+  `,
+  methods: {
+    toggle(){
+      this.open = !this.open
+    }
+  }
+})
